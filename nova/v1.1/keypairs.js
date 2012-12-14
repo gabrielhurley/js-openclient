9193d49d623d099872476ffd586156130545eca0
Gabriel Hurley <gabriel@strikeawe.com>
2012-12-13 16:32:19 -0800

    Set the id on the result of keypair create call.

--- nova/v1.1/keypairs.js
+++ nova/v1.1/keypairs.js
@@ -7,11 +7,14 @@ var KeypairManager = base.Manager.extend({
   plural: "keypairs",
   singular: "keypair",
 
+  // Keypairs don't return their ID via the API, so we use the name instead.
   all: function (params) {
     var manager = this;
     params.parseResult = function (result) {
       var modified_results = [];
       result.forEach(function (item) {
+        // The keypairs list call returns an extra layer of wrapping around
+        // each keypair which we need to remove.
         var modified_item = item[manager.singular];
         modified_item.id = modified_item.name;
         modified_results.push(modified_item);
@@ -20,6 +23,14 @@ var KeypairManager = base.Manager.extend({
     };
     return this._super(params);
   },
+  create: function (params) {
+    params.parseResult = function (result) {
+      result.id = result.name;
+      return result;
+    };
+    return this._super(params);
+  },
+
   get: function (params) { throw error.NotImplemented; },
   update: function (params) { throw error.NotImplemented; }
 });
