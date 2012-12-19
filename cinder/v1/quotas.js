ec44969afa7a778eabbc8e33d0dffc376f0018fc
Gabriel Hurley <gabriel@strikeawe.com>
2012-12-18 21:52:47 -0800

    Adds quota usage methods.

--- cinder/v1/quotas.js
+++ cinder/v1/quotas.js
@@ -6,7 +6,31 @@ var base = require("../../client/base"),
 var QuotaManager = base.Manager.extend({
   namespace: "os-quota-sets",
   singular: 'quota_set',
-  plural: 'quota_sets'// TODO(roland): is this right?
+  plural: 'quota_sets',
+
+  usages: function (params) {
+    var usages = {},
+        flavors = {},
+        volumes = [];
+
+    usages.id = this.client.tenant.id;
+    usages.gigabytes = 0;
+    usages.disk = 0;
+
+    this.client.volumes.all({
+      detail: true,
+      success: function (volumes) {
+        volumes.forEach(function (volume) {
+          usages.gigabytes += volume.size;
+          usages.disk += volume.size;
+        });
+        return params.success(usages);
+      },
+      error: function (err) {
+        return params.error(err);
+      }
+    });
+  }
 });
 
 
--- nova/v1.1/quotas.js
+++ nova/v1.1/quotas.js
@@ -1,4 +1,5 @@
-var base = require("../../client/base"),
+var async = require('async'),
+    base = require("../../client/base"),
     error = require("../../client/error");
 
 // TODO(roland): Make methods throw NotImplemented where applicable.
@@ -6,7 +7,56 @@ var base = require("../../client/base"),
 var QuotaManager = base.Manager.extend({
   namespace: "os-quota-sets",
   singular: 'quota_set',
-  plural: 'quota_sets'// TODO(roland): is this right?
+  plural: 'quota_sets',
+
+  usages: function (params) {
+    var manager = this,
+        usages = {},
+        flavors = {},
+        instances = [];
+
+    usages.id = this.client.tenant.id;
+    usages.cores = 0;
+    usages.vcpus = 0;
+    usages.ram = 0;
+
+    async.parallel([
+      function (next) {
+        manager.client.servers.all({detail: true}, function (err, results) {
+          if (err) {
+            return next(err);
+          }
+          instances = results;
+          next();
+        });
+      },
+      function (next) {
+        manager.client.flavors.all({detail: true}, function (err, results) {
+          if (err) {
+            return next(err);
+          }
+          results.forEach(function (flavor) {
+            flavors[flavor.id] = flavor;
+          });
+          next();
+        });
+      }
+    ], function (err) {
+      if (err && params.error) {
+        return params.error(err);
+      }
+
+      instances.forEach(function (instance) {
+        var flavor = flavors[instance.flavor.id];
+
+        usages.vcpus += flavor.vcpus;
+        usages.cores += flavor.vcpus;
+        usages.ram += flavor.ram;
+      });
+
+      return params.success(usages);
+    });
+  }
 });
 
 
