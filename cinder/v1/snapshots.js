8836e10ab44e81c15bdac10986c10613e0382e97
Gabriel Hurley <gabriel@strikeawe.com>
2013-08-02 16:23:00 -0700

    Allow fetching volume snapshot detailed list.

--- cinder/v1/snapshots.js
+++ cinder/v1/snapshots.js
@@ -4,7 +4,21 @@ var base = require("../../client/base"),
 // TODO(roland): Make methods throw NotImplemented where applicable.
 
 var SnapshotManager = base.Manager.extend({
-  namespace: "snapshots"
+  namespace: "snapshots",
+
+  get_base_url: function (params) {
+    var base_url = this._super(params);
+    if (params.detail) {
+      base_url = this.urljoin(base_url, 'detail');
+    }
+    return base_url;
+  },
+
+  all: function (params, callback) {
+    if (typeof params.detail === "undefined") params.detail = true;
+    return this._super(params, callback);
+  }
+
 });
 
 
